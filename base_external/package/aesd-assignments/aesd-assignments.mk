a291f4ece0783d27657c756d7462ff7a20233b35
Adil Chatha <adilahmad619@gmail.com>
2025-12-03 22:26:00 -0600

    Assignment 5 part 2 complete. AC

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,37 +4,48 @@
 #
 ##############################################################
 
-# Use the specific commit hash you want to build (from your assignment 3 repo)
-AESD_ASSIGNMENTS_VERSION = e808f3a87c0cb3b8104fb084fb7880cccf0ad9a4
-
-
-# SSH URL to your assignment-3-and-later repo
+AESD_ASSIGNMENTS_VERSION = 1eff5e13ac4921d280913ef98e8a302f06d3c48a
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-adil619.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
-
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 ###############################################################
 # Build Commands
 ###############################################################
+
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 ###############################################################
 # Install Commands
 ###############################################################
+
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+
 	# Install writer binary
-	$(INSTALL) -D -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/writer
+	$(INSTALL) -D -m 0755 $(@D)/finder-app/writer \
+		$(TARGET_DIR)/usr/bin/writer
 
 	# Install shell scripts
-	$(INSTALL) -D -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/finder.sh
-	$(INSTALL) -D -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
+	$(INSTALL) -D -m 0755 $(@D)/finder-app/finder.sh \
+		$(TARGET_DIR)/usr/bin/finder.sh
+	$(INSTALL) -D -m 0755 $(@D)/finder-app/finder-test.sh \
+		$(TARGET_DIR)/usr/bin/finder-test.sh
 
 	# Install configuration directory
 	$(INSTALL) -d $(TARGET_DIR)/etc/finder-app/conf
 	cp -r $(@D)/finder-app/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+
+	# Install aesdsocket binary
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket \
+		$(TARGET_DIR)/usr/bin/aesdsocket
+
+	# Install aesdsocket-start-stop script
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop \
+		$(TARGET_DIR)/etc/init.d/S99aesdsocket
+
 endef
 
 $(eval $(generic-package))
